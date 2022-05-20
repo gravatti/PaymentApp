361ad0433bc8b0b9eaf45e3ca9d4c1a57ce081b4
Ishmael Gravatt <ishmaelgravatt@Ishmaels-MacBook-Pro.local>
2022-05-19 22:15:13 -0700

    my commit 2

--- src/main/java/question2/FunctionTest.java
+++ src/main/java/question2/FunctionTest.java
@@ -4,13 +4,36 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class FunctionTest {
-  public static String test = "";
+  public static StringTester dupeTest = new StringTester() {
+    @Override
+    public boolean testString(String input) {
+      HashSet<Character> string = new HashSet<>();
+      for (int i = 0; i < input.length(); i++){
+        string.add(input.charAt(i));
+      }
+      return !(string.size() == input.length());
+    }
+  };
+
+  public static StringTester capsTest = new StringTester() {
+    @Override
+    public boolean testString(String input) {
+     Boolean capsFound = false;
+     for (int i = 0; i < input.length(); i++){
+       if (Character.isUpperCase(input.charAt(i))){
+         capsFound = true;
+         break;
+       }
+     }
+      return capsFound;
+    }
+  };
 
   public static StringTester hasDuplicateTester(){
-    return null;
+    return dupeTest;
   }
 
   public static StringTester hasCapsChecker() {
-    return null;
+    return capsTest;
   }
 }
